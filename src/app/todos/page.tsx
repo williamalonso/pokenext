a81f45c2a42eedd0452f56168c492b708e4b24ba
William Alonso <williamalonso@outlook.com>
2023-11-08 23:43:56 -0300

    Fetch Data w App Router

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,35 +1,29 @@
-// import React from "react";
-// import styles from '../../styles/Todo.module.css';
+import React from "react";
+import styles from '../../styles/Todo.module.css';
 
-// interface TodosProps {
-//   data: any[]; // Defina o tipo de 'data' como um array de qualquer tipo
-// }
+interface todoProps {
+  id: number;
+  title: string;
+}
 
-// export async function getStaticProps() {
-//   // Realize uma solicitação de busca de dados
-//   const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-//   const data = await res.json();
-
-//   return {
-//     props: {
-//       data, // Os dados que você deseja passar para a página
-//     },
-//   };
-// }
-
-// const Todos: React.FC<TodosProps> = ( {data} ) => {
-//   return (
-//     <>
-//       <h1>Tarefas para fazer: </h1>
-//       <ul className={ styles.todolist }>
-//         {
-//           data.map( (item) => (
-//             <li>{item.title}</li>
-//           ))
-//         }
-//       </ul>
-//     </>
-//   );
-// }
- 
-// export default Todos;
+export default async function Todos() {
+  
+  const rest = await fetch('http://localhost:3000/api');
+  const data = await rest.json();
+  console.log(rest);
+  return (
+    <>
+      <h1>Tarefas para fazer: </h1>
+      <ul className={ styles.todolist }>
+        {
+          data.map( (item: todoProps) => (
+            <div key={item.id}>
+              <span>Item {item.id}:</span> 
+              <li>{item.title}</li>
+            </div>
+          ))
+        }
+      </ul>
+    </>
+  );
+}
