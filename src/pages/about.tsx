3e940f005d188dc994e0237d92a68c23b7d49463
William Alonso <williamalonso@outlook.com>
2023-11-12 01:40:25 -0300

    navbar & footer

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import '../styles/globals.css';
+import Image from "next/image";
 
 const About: React.FC = () => {
   return ( 
-    <div>
-      <h1>página about page router</h1>
+    <div className="text-center">
+      <h1 className="text-[2em] mb-[1em]">Sobre o projeto</h1>
+      <p className="mt-0 mb-[1.5rem] mx-auto max-w-[500px] leading-8">
+        Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+      </p>
+      <Image src="/images/charizard.png" width={300} height={293} alt="Imagem do Charizard" className="my-0 mx-auto" />
     </div>
   );
 }
--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-center items-center bg-[#333] h-[150px] mt-[2em] border-t-[3px] border-solid border-[#111]">
+    <footer className="flex justify-center items-center bg-[#333] h-[150px] mt-[2em] border-t-[3px] border-solid border-[#111] text-[#fff]">
       <p>
         <span className="font-bold">PokeNext</span> &copy; 2023
       </p>
