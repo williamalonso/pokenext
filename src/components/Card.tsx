f106a06bf225f8318fa5a8106a515057a8e1af51
William Alonso <williamalonso@outlook.com>
2023-11-15 07:05:46 +0000

    style card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import CardProps from '@/interfaces/cardType';
+import Link from 'next/link';
 import Image from 'next/image';
+import CardProps from '@/interfaces/cardType';
+import style from '../styles/Card.module.css';
 
 const Card: React.FC<CardProps> = ( props ) => {
 
   // console.log(props);
 
   return (
-    <div>
+    <div className={style.card}>
       <Image src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${props.id + 1}.svg`} width="120" height="120" style={{ height: 'auto' }} alt="Pokemon" />
-      <h3>
+      <p className={style.id}>
+        {props.id + 1}
+      </p>
+      <h3 className={style.title}>
         {props.data}
       </h3>
+      <Link href="#" className={style.btn}>
+        Detalhes
+      </Link>
     </div>
   );
 
